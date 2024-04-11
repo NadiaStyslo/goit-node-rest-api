9a71598a00d99b7528f5715e4e9bf3c5c5634644
NadiaStyslo <sir.ihor@gmail.com>
2024-04-11 13:08:19 -0400

    1

--- DB_HOST.js
+++
@@ -1,2 +0,0 @@
-export const { DB_HOST } =
-  'mongodb+srv://NStyslo:Anna8140216@cluster0.04fujrz.mongodb.net/db-contacts?retryWrites=true&w=majority&appName=Cluster0';
--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 import express, { query } from 'express';
 import morgan from 'morgan';
 import mongoose from 'mongoose';
-import dotenv from 'dotenv';
+//import dotenv from 'dotenv';
 
 import cors from 'cors';
 
 import contactsRouter from './routes/contactsRouter.js';
 
 const app = express();
-dotenv.config();
+//dotenv.config();
 
 const DB_HOST =
   'mongodb+srv://NStyslo:Anna8140216@cluster0.04fujrz.mongodb.net/db-contacts?retryWrites=true&w=majority&appName=Cluster0';
